e5c643247308b1a2be542a2eb8d3bb9ee682bcbd
Eric Byers <eric.byers@chaoticmoon.com>
2014-12-11 11:06:24 -0600

    Removing create-alert route that is not needed in the UX

--- assets/js/app/common/routes.js
+++ assets/js/app/common/routes.js
@@ -4,15 +4,6 @@
 module.exports = function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, USER_ROLES) {
   $urlRouterProvider.otherwise("/");
 
-  $stateProvider
-    // create alert
-    .state('base.createAlert', {
-      url: "^/create-alert",
-      templateUrl: "/partials/create-alert.html",
-      data: {
-        authorizedRoles: [USER_ROLES.user, USER_ROLES.admin]
-      }
-    });
   $locationProvider.html5Mode(true);
 
   $httpProvider.interceptors.push('httpInterceptor');
