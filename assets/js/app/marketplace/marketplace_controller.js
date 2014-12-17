c10745d6f5d5649b010891c690bd6c411e911b6b
Eric Byers <eric.byers@chaoticmoon.com>
2014-12-17 12:57:54 -0600

    Comments and filter the data to remove deleted items

--- assets/js/app/marketplace/marketplace_controller.js
+++ assets/js/app/marketplace/marketplace_controller.js
@@ -4,8 +4,12 @@ var _ = require('lodash');
 
 /**@ngInject*/
 function MarketplaceController($scope, products, categories) {
-  this.products = products;
+
   this.categories = categories;
+  // Only display non deleted products.
+  this.products = _.filter(products, function(product) {
+    return product.deleted_at === null;
+  });
 
   _.each(this.categories, _.bind(function(category) {
     category.products = _.filter(this.products, function(product) {
@@ -14,6 +18,10 @@ function MarketplaceController($scope, products, categories) {
   }, this));
 }
 
+/**
+ * @todo categories, products are used in product admin as well, could be
+ *       combined better in the future.
+ */
 MarketplaceController.resolve = {
   /**@ngInject*/
   categories: function(ProductCategory) {
