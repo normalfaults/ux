1a0fead2eec87908ef475a0fac15b8387fe9617b
Christopher Parratto <cparratto@gmail.com>
2014-12-17 15:32:38 -0600

    More changes based on comments in PR.

--- assets/js/app/admin/projects/new_project_question_controller.js
+++ assets/js/app/admin/projects/new_project_question_controller.js
@@ -12,13 +12,14 @@ function NewProjectQuestionController($scope, $state, ProjectQuestion) {
         if(newType != lastType && newType === 'select_option') {
             $scope.newProjectQuestion.options = ['']
         } else if(newType != lastType && $scope.newProjectQuestion.options) {
-            delete $scope.newProjectQuestion[options]
+            $scope.newProjectQuestion.options = null
         }
     });
 
     $scope.createProjectQuestion = function(){
+
         if ($scope.projectQuestionForm.$valid) {
-            ProjectQuestion.save($scope.newProjectQuestion, function() {
+            ProjectQuestion.save($scope.newProjectQuestion.options ? $scope.newProjectQuestion : _.omit($scope.newProjectQuestion, 'options'), function() {
                 $state.go('base.admin.projects.project_questions', {}, {reload: true});
             });
         }
