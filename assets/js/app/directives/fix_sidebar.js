8dc7f707e943829abf76e661ea9706edfbaf3547
Eric Byers <eric.byers@chaoticmoon.com>
2014-12-17 15:40:02 -0600

    Defer the sidebar updates to allow that to mostly work, pass in currentUser to dashboard scope since it is used there

--- assets/js/app/directives/fix_sidebar.js
+++ assets/js/app/directives/fix_sidebar.js
@@ -1,18 +1,21 @@
 'use strict';
 
 /**@ngInject*/
-function FixSidebar(fixSidebar) {
+function FixSidebar(fixSidebar, $timeout) {
   return {
     restrict: 'A',
-    link: function(scope, element) {
-      scope.$watch(function() {
-        return $(element).height();
+    link: function($scope, $element) {
+      $scope.$watch(function() {
+        return $element.height();
       }, function(newValue, oldValue) {
         if (newValue !== oldValue) {
           fixSidebar();
         }
       });
-      element.ready(function() {
+
+      // @todo This is a hack, but it gets us out of the render call and defers it till after the DOM
+      // is done loading, wich lets the sidebar/footer be set correctly.
+      $timeout(function() {
         fixSidebar();
       });
     }
