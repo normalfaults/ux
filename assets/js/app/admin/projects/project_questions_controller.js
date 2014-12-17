3240dd2a329f2b6b3cefe6ba2b111e31cb7905b8
Christopher Parratto <cparratto@gmail.com>
2014-12-17 14:24:51 -0600

    Cleanup based on comments in PR.

--- assets/js/app/admin/projects/project_questions_controller.js
+++ assets/js/app/admin/projects/project_questions_controller.js
@@ -2,36 +2,33 @@
 
 /**@ngInject*/
 function ProjectQuestionsController($scope, $state, projectQuestions, ProjectQuestion) {
-    var scope = $scope,
-        state = $state;
-
     $scope.projectQuestions = projectQuestions
     $scope.newProjectQuestion = {}
 
     $scope.addOption = function() {
-        scope.newProjectQuestion.options.push('')
+        $scope.newProjectQuestion.options.push('')
     }
 
     $scope.$watch('newProjectQuestion.field_type', function(newType, lastType) {
         if(newType != lastType && newType === 'select_option') {
             $scope.newProjectQuestion.options = ['']
-        } else if(newType != lastType && scope.newProjectQuestion.options) {
-            delete scope.newProjectQuestion[options]
+        } else if(newType != lastType && $scope.newProjectQuestion.options) {
+            delete $scope.newProjectQuestion[options]
         }
     });
 
     $scope.createProjectQuestion = function(){
         if (scope.projectQuestionForm.$valid) {
 
-            ProjectQuestion.save(scope.newProjectQuestion, function() {
-                state.go('base.admin.projects.project_questions', {}, {reload: true});
+            ProjectQuestion.save($scope.newProjectQuestion, function() {
+                $state.go('base.admin.projects.project_questions', {}, {reload: true});
             });
         }
     }
 
     $scope.deleteProjectQuestion = function(question){
         question.$delete(function() {
-            state.go('base.admin.projects.project_questions', {}, {reload: true});
+            $state.go('base.admin.projects.project_questions', {}, {reload: true});
         });
     }
 }
