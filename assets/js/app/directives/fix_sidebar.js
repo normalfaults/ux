f3ec4e73a92e9a1d218d05a206462360f06fdce7
Michael Stack <michael.stack@gmail.com>
2014-12-22 15:33:31 -0600

    Remove the dependency on the JS fix-sidebar directive
    - Footer sticks to the bottom of the window
    - Or it will stick to content longer than the window

--- assets/js/app/directives/fix_sidebar.js
+++ assets/js/app/directives/fix_sidebar.js
@@ -3,22 +3,22 @@
 /**@ngInject*/
 function FixSidebar(fixSidebar, $timeout) {
   return {
-    restrict: 'A',
-    link: function($scope, $element) {
-      $scope.$watch(function() {
-        return $element.height();
-      }, function(newValue, oldValue) {
-        if (newValue !== oldValue) {
-          fixSidebar();
-        }
-      });
-
-      // @todo This is a hack, but it gets us out of the render call and defers it till after the DOM
-      // is done loading, wich lets the sidebar/footer be set correctly.
-      $timeout(function() {
-        fixSidebar();
-      });
-    }
+    //restrict: 'A',
+    //link: function($scope, $element) {
+    //  $scope.$watch(function() {
+    //    return $element.height();
+    //  }, function(newValue, oldValue) {
+    //    if (newValue !== oldValue) {
+    //      fixSidebar();
+    //    }
+    //  });
+    //
+    //  // @todo This is a hack, but it gets us out of the render call and defers it till after the DOM
+    //  // is done loading, wich lets the sidebar/footer be set correctly.
+    //  $timeout(function() {
+    //    fixSidebar();
+    //  });
+    //}
   };
 }
 
