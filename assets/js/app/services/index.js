ea4e9bae0f25c20fcb2a48246223285d0f2df249
Eric Byers <eric.byers@chaoticmoon.com>
2014-12-23 16:07:31 -0600

    Last of the renames hopefully

--- assets/js/app/services/index.js
+++ assets/js/app/services/index.js
@@ -4,7 +4,7 @@ var angular = require('angular');
 
 var ServicesModule = angular.module('broker.services', [])
   .controller('ServiceController', require('./service_controller'))
-  .factory('Service', require('./service'))
+  .factory('ServiceResource', require('./service_resource'))
   .config(require('./routes'));
 
 module.exports = ServicesModule;
