b856bb20d485d1b7b9eb00aa0972c463289a34f3
Christopher Parratto <cparratto@gmail.com>
2014-12-29 10:46:59 -0500

    Added type-o causing build problems.

--- assets/js/app/projects/project_user.js
+++ assets/js/app/projects/project_user.js
@@ -2,7 +2,7 @@
 
 /**@ngInject*/
 var ProjectUser = function($resource, ApiResource) {
-    return $resource(ApiResource('projectUser'), {id:'@id', staff_id:'@staff_id'}, {});r;
+    return $resource(ApiResource('projectUser'), {id:'@id', staff_id:'@staff_id'}, {});
 };
 
 module.exports = ProjectUser;
