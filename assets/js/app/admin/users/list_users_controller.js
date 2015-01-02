1f6976c3cabc511e3eda9739ac082d83e4535336
Eric Byers <eric.byers@chaoticmoon.com>
2015-01-02 14:35:40 -0600

    Hooking up removing of a user

--- assets/js/app/admin/users/list_users_controller.js
+++ assets/js/app/admin/users/list_users_controller.js
@@ -3,7 +3,9 @@
 var _ = require('lodash');
 
 /**@ngInject*/
-var ListUsersController = function(users) {
+var ListUsersController = function(UsersResource, users) {
+
+  this.UsersResource = UsersResource;
 
   this.users = users;
 
@@ -14,6 +16,24 @@ ListUsersController.resolve = {
   users: function(UsersResource) {
     return UsersResource.get().$promise;
   }
+};
+
+ListUsersController.prototype = {
+
+  removeUser: function(userKey) {
+
+    var userId = this.users[userKey].id;
+    this.UsersResource.delete({id: userId}).$promise.then(
+      _.bind(function() {
+        this.users.splice(userKey, 1);
+      }, this),
+      function() {
+        // @todo Errors should be moved more global.
+        alert('Error removing users');
+      }
+    );
+  }
+
 };
 
 module.exports = ListUsersController;
