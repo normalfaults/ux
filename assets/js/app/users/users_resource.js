36317acf3e1043d4c51375619b14dccb374df159
Eric Byers <eric.byers@chaoticmoon.com>
2015-01-06 16:58:21 -0600

    Combining UsersResource and UserResource

--- assets/js/app/users/users_resource.js
+++ assets/js/app/users/users_resource.js
@@ -2,7 +2,13 @@
 
 /**@ngInject*/
 var UsersResource = function($resource, ApiResource) {
-  return $resource(ApiResource('staffById'), {'id': '@id'}, {
+  var Users = $resource(ApiResource('staffById'), {'id': '@id'}, {
+    // Get Current
+    getCurrentMember: {
+      method: 'GET',
+      isArray: false,
+      url: ApiResource('currentMember')
+    },
     // Get Single
     get: {
       method: 'GET',
@@ -17,6 +23,21 @@ var UsersResource = function($resource, ApiResource) {
       method: 'PUT'
     }
   });
+
+  /**
+   * @todo This should probably be rethought a bit, since fullName/isAdmin
+   *       can be called on a collection, not just a single item.
+   */
+
+  Users.prototype.fullName = function() {
+    return [this.first_name, this.last_name].join(' ');
+  };
+
+  Users.prototype.isAdmin = function() {
+    return this.role === 'admin';
+  };
+
+  return Users;
 };
 
 module.exports = UsersResource;
