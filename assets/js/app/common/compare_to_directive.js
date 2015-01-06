581370dfff2883dffba260bab421ae5e5beccd05
Eric Byers <eric.byers@chaoticmoon.com>
2015-01-06 15:20:18 -0600

    Adding password confirm, directive to compare with validation

---
+++ assets/js/app/common/compare_to_directive.js
@@ -0,0 +1,25 @@
+/**
+ * Allows comparing of two different valus against each other.
+ *
+ * @see http://odetocode.com/blogs/scott/archive/2014/10/13/confirm-password-validation-in-angularjs.aspx
+ */
+var compareTo = function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+};
+
+module.exports = compareTo;
