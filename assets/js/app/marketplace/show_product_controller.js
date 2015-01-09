63ebe04cc4e926c6fadc003e8ed764058c4d3c30
Eric Byers <eric.byers@chaoticmoon.com>
2015-01-09 13:26:09 -0600

    Adding method to change the back link dependent on where the page came from

--- assets/js/app/marketplace/show_product_controller.js
+++ assets/js/app/marketplace/show_product_controller.js
@@ -1,9 +1,32 @@
 'use strict';
 
 /**@ngInject*/
-function ShowProductController(product) {
+var ShowProductController = function($state, $scope, product) {
+
+  this.$state = $state;
+
   this.product = product;
-}
+
+  // Default 'Back' is going to the marketplace list.
+  this.backTo = 'marketplace';
+  this.backToParams = {};
+
+  /**
+   * If we come to this page from the order detail page, we set the back to to orderDetail, and set the parameters.
+   * @todo This is a bit ugly since the previousRoute does not include its state, so we have to check based on the URL
+   *       and then the state is hard coded in the template.
+   */
+  $scope.$on('$stateChangeSuccess', _.bind(function($stateChangeSuccess, newRoute, newRouteParams, previousRoute, previousRouteParams) {
+      if (previousRoute.url == "/orders/:id") {
+        this.backTo = 'orderDetail';
+        this.backToParams = previousRouteParams;
+      }
+  }, this));
+};
+
+ShowProductController.prototype = {
+
+};
 
 ShowProductController.resolve = {
   /**@ngInject*/
