7eb76a9edd9745e1ad6beae900955d4058cba66a
Eric Byers <eric.byers@chaoticmoon.com>
2015-01-09 13:39:38 -0600

    Moving it globally, less URL hacking

--- assets/js/app/base/base_controller.js
+++ assets/js/app/base/base_controller.js
@@ -7,6 +7,18 @@ var BaseController = function($rootScope, $scope, $state, projects, AuthService)
   $rootScope.$state = $state;
   $rootScope.projects = projects;
 
+  /**
+   * Track the previous state and the parameters.  Helpful for use in controllers
+   * which may be accessed from two different states.
+   *
+   * @todo Potentially store this data so on a page refresh we can set the state back correctly for the user.
+   */
+  $rootScope.previousState;
+  $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+    $rootScope.previousState = fromState.name;
+    $rootScope.previousStateParams = fromParams;
+  });
+
   angular.forEach(projects, function(project) {
     project.sref = "base.project(" + angular.toJson({projectId: project.id}) + ")";
   });
--- assets/js/app/marketplace/show_product_controller.js
+++ assets/js/app/marketplace/show_product_controller.js
@@ -1,27 +1,15 @@
 'use strict';
 
 /**@ngInject*/
-var ShowProductController = function($state, $scope, product) {
+var ShowProductController = function($state, $rootScope, product) {
 
   this.$state = $state;
 
   this.product = product;
 
-  // Default 'Back' is going to the marketplace list.
-  this.backTo = 'marketplace';
-  this.backToParams = {};
 
-  /**
-   * If we come to this page from the order detail page, we set the back to to orderDetail, and set the parameters.
-   * @todo This is a bit ugly since the previousRoute does not include its state, so we have to check based on the URL
-   *       and then the state is hard coded in the template.
-   */
-  $scope.$on('$stateChangeSuccess', _.bind(function($stateChangeSuccess, newRoute, newRouteParams, previousRoute, previousRouteParams) {
-      if (previousRoute.url == "/orders/:id") {
-        this.backTo = 'orderDetail';
-        this.backToParams = previousRouteParams;
-      }
-  }, this));
+  this.backTo = $rootScope.previousState;
+  this.backToParams = $rootScope.previousStateParams;
 };
 
 ShowProductController.prototype = {
