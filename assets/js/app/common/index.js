b7167f8f34b4e86d319717234c4745d286b74517
Christopher Parratto <cparratto@gmail.com>
2015-01-10 22:02:21 -0600

    Forgot to add xsrf interceptor. Standard angular xsrf does not work w/ cors.

--- assets/js/app/common/index.js
+++ assets/js/app/common/index.js
@@ -8,6 +8,7 @@ module.exports = angular.module('broker.common', [
 ])
   .factory('ApiResource', require('./api_resource'))
   .factory('httpInterceptor', require('./http_interceptor'))
+  .factory('xsrfInterceptor', require('./xsrf_interceptor'))
   .factory('JellyfishModal', require('./jellyfish_modal'))
   .directive('sectionHeader', require('./section_header_directive'))
   .directive('compareTo', require('./compare_to_directive'))
