61b05368992957e66de571228d0d45c8bd48d8e4
Eric Byers <eric.byers@chaoticmoon.com>
2015-01-16 15:39:48 -0600

    Removing fix boxes height directive

--- assets/js/app/directives/index.js
+++ assets/js/app/directives/index.js
@@ -9,7 +9,6 @@ var DirectivesModule = angular.module('broker.directives', [])
   .directive('expandArrow', require('./expand_arrow'))
   .directive('setWidth', require('./set_width'))
   .directive('toggleSidebar', require('./toggle_sidebar'))
-  .directive('fixBoxesHeight', require('./fix_boxes_height'))
   .directive('formInput', require('./form_input'))
   .directive('switchViewButtons', require('./switch_view_buttons'));
 
