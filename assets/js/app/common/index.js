3005143c8665c07d958e95fd3a361944680e04f8
Michael Stack <michael.stack@gmail.com>
2015-02-26 16:18:40 -0600

    Remove XSRF Intercept
    - This does nothing for UX. When UX and API are on different domains (or ports) cookies aren't available.

--- assets/js/app/common/index.js
+++ assets/js/app/common/index.js
@@ -8,7 +8,6 @@ module.exports = angular.module('broker.common', [
 ])
   .factory('apiResource', require('./api_resource'))
   .factory('httpInterceptor', require('./http_interceptor'))
-  .factory('xsrfInterceptor', require('./xsrf_interceptor'))
   .factory('JellyfishModal', require('./jellyfish_modal'))
   .directive('sectionHeader', require('./section_header_directive'))
   .directive('compareTo', require('./compare_to_directive'))
